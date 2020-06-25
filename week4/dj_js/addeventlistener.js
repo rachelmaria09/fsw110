4265a521beece33847d248b8bac0aa07e37b2e66
rachelmaria09 <rmgildea@gmail.com>
2020-06-25 16:23:42 -0400

    second

--- week4/dj_js/addeventlistener.js
+++ week4/dj_js/addeventlistener.js
@@ -1,19 +1,25 @@
 var square = document.getElementById("square");
+//Turn square blue with 'mouseover'.
 document.getElementById("square").addEventListener("mouseover", function() {
     square.style.backgroundColor = "blue";
 });
+//Turn square red with 'mousedown'.
 document.getElementById("square").addEventListener("mousedown", function() {
     square.style.backgroundColor = "red";
 });
+//Turn square yellow with 'mouseup'.
 document.getElementById("square").addEventListener("mouseup", function() {
     square.style.backgroundColor = "yellow";
 });
+//Turn square green with 'dblclick'.
 document.getElementById("square").addEventListener("dblclick", function() {
     square.style.backgroundColor = "green";
 });
+//Turn square orange with 'wheel'.
 window.addEventListener("wheel", function() {
     square.style.backgroundColor = "orange";
 });
+//Turn square either blue, red, yellow, green, or orange when first letter of color is typed.
 window.addEventListener("keypress", checkKeyPress, false); 
 function checkKeyPress(key) {
     if(key.keyCode == "114") {
